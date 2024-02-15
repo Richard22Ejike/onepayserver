424ce7b6832282a22f03a37c2168f5da5debd9ea
Richard dev <richard.ekene@aun.edu.ng>
2024-02-15 12:35:09 +0100

    second

--- bases/settings.py
+++ bases/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6dwsxiz_t4nosfs+f9yzqobr(_cy5_ah&@1ho2jk2&u=*s1^p4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
